f5b171261d3be294e9dc8a35407386bff26293ef
ltrgoddard <louisgoddard@gmail.com>
2014-07-06 12:03:52 +0100

    Update chiastic.py

--- chiastic.py
+++ chiastic.py
@@ -4,7 +4,7 @@
 # by Louis Goddard <l.goddard@sussex.ac.uk>
 
 # This short script is designed to detect basic chiastic sentence structures (A, B, B, A) in
-# plain text files. Usage: "python chiastic.py ['-s' for single mode, '-b' for bulk mode']
+# plain text files. Usage: "python chiastic.py ['-s' for single mode, '-b' for bulk mode]
 # [word limit as integer] [file to search (single mode only)]". To specify words to ignore,
 # create a list called 'ignore.txt' in the same directory as this script. To use in bulk mode,
 # create a list of target files called 'targets.txt' (when running in bulk mode, Chiastic will
